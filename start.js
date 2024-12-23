9934ff99ac0d92b5e10f43ad1157451245e675f7
marzley <146063946+marzley@users.noreply.github.com>
2024-12-23 23:48:24 +0300

    Update start.js

--- start.js
+++ start.js
@@ -5,13 +5,12 @@
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATHMD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name Marzley 
+* @authors Sir Kelvin & Marzley Tech solutions 
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
-* @link <https://github.com/prabathLK/PRABATHMD>
 * @version 2.1.3
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2025 Kelvin tech, MARZLEY-BOT. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
